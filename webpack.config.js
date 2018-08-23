bafd416eb227e250046dd4c6836284cdcb7f8295
liuhuihui <244701374@qq.com>
2018-08-23 17:05:09 +0800

    babel 箭头函数编译 及 less

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-    entry: './src/app.js',  //入口
+    entry: './src/index.js',  //入口
     output: {   //输出
         path: path.resolve(__dirname, 'dist'),
         // publicPath: "assert/",  //解决路径，引用从根路径开始
@@ -18,7 +18,7 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['env', 'react']
+                        presets: ['env', 'react','es2015','stage-0']
                     }
                 }
             },{
@@ -27,11 +27,19 @@ module.exports = {
                     fallback: "style-loader",
                     use: "css-loader"
                 })
-            },{
+            },
+            /*{
                 test: /\.scss$/, //sass文件处理
                 use: ExtractTextPlugin.extract({
                     fallback: "style-loader",
                     use: ['css-loader','sass-loader']
+                })
+            },*/
+            {
+                test: /\.less$/,  //less文件处理
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ['css-loader','less-loader']
                 })
             },{
                 test: /\.(png|jpg|gif)$/,  //图片的配置
@@ -63,14 +71,10 @@ module.exports = {
             template: './src/index.html'
         }),
         // new ExtractTextPlugin('./[name].css'),  //独立css
-        new ExtractTextPlugin('style.css'),
-        new webpack.optimize.CommonsChunkPlugin({  //公共模块
-            name:'common',
-            filename:'js/base.js'
-        })
+        new ExtractTextPlugin('assert/style.css'),
     ],
     devServer: {
         port:'5201',
-       contentBase: './dist'
+        contentBase: './dist'
     },
 };
