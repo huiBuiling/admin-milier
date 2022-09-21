8b7b1015ffb7c7d38b66d541f910f0b7d6d50f7a
慧慧 <244701374@qq.com>
2022-09-21 11:25:20 +0800

    颜色调整

--- README.md
+++ README.md
@@ -1,117 +1,133 @@
 # admin-milier
+
 antd + react 后台管理
 
 > 部分前端设计效果均是参照可爱的设计小姐姐和小哥哥们的风格，仅个人学习使用哦
 
-###### 启动：yarn run dev
-###### 安装：yarn
+######
+
+######
+
+- 安装：yarn + yarn dev:dll
+- 启动：yarn start
+
 ###### 访问：http://localhost:5201/
 
- - 非常感谢提供的音乐接口（仅供学习使用，如有侵权请联系）
- - https://binaryify.github.io/NeteaseCloudMusicApi/#/?id=neteasecloudmusicapi
+- 非常感谢提供的音乐接口（仅供学习使用，如有侵权请联系）
+- https://binaryify.github.io/NeteaseCloudMusicApi/#/?id=neteasecloudmusicapi
 
 ###### 体验音乐功能需
+
     - 下载：git clone https://github.com/Binaryify/NeteaseCloudMusicApi.git
     - 运行: node app.js
 
 ### topbar
+
 > 获取网易云音乐中设置的用户名和用户头像
-> 换肤(默认夜间配色， 支持svg配色哦)
+> 换肤(默认夜间配色， 支持 svg 配色哦)
 > 瀑布流图片（支持音乐播放，下一首，上一首，喜欢未实现）
 > 消息通知（未）
 > 登录，退出
 
 ### leftbar
+
 - 首页
-![image](https://user-images.githubusercontent.com/20200628/98806668-7ee93e80-2454-11eb-9c05-1016208c214d.png)
+  ![image](https://user-images.githubusercontent.com/20200628/98806668-7ee93e80-2454-11eb-9c05-1016208c214d.png)
 - 介绍
 - 图表：气泡图，饼状图，雷达图（线性图，柱状图已做封装，见首页），水球图
-    - 水球图：yarn add echarts-liquidfill
-    
+  - 水球图：yarn add echarts-liquidfill
 - 记事
 
-    ```
-        https://www.npmjs.com/package/localdb
-        import LocalDb from "localDb";
-        let Notes = new localdb('notes', 'Array', true);
-        this.db.add(item);  //只能是对象，感觉和以前不一样了，弃用
-    ```
+  ```
+      https://www.npmjs.com/package/localdb
+      import LocalDb from "localDb";
+      let Notes = new localdb('notes', 'Array', true);
+      this.db.add(item);  //只能是对象，感觉和以前不一样了，弃用
+  ```
 
 - 地图
 
 - 会议
-![image](https://user-images.githubusercontent.com/20200628/98076231-dca3e680-1ea8-11eb-964f-f3cd76c19479.png)
+  ![image](https://user-images.githubusercontent.com/20200628/98076231-dca3e680-1ea8-11eb-964f-f3cd76c19479.png)
 
 - 物业
-![image](https://user-images.githubusercontent.com/20200628/98807061-1353a100-2455-11eb-95ec-68391c9652f5.png)
+  ![image](https://user-images.githubusercontent.com/20200628/98807061-1353a100-2455-11eb-95ec-68391c9652f5.png)
 
-- 仿网易云音乐   
-![image](https://user-images.githubusercontent.com/20200628/98807132-2bc3bb80-2455-11eb-8d30-319104b3faca.png)
+- 仿网易云音乐  
+  ![image](https://user-images.githubusercontent.com/20200628/98807132-2bc3bb80-2455-11eb-8d30-319104b3faca.png)
 
 - 拖曳
-    - [x] github:https://github.com/bevacqua/dragula
-    - [x] npm install dragula --save
-    - [x] 使用时拖动会复制出对应的 ，记得列表添加类名控制样式
+
+  - [x] github:https://github.com/bevacqua/dragula
+  - [x] npm install dragula --save
+  - [x] 使用时拖动会复制出对应的 ，记得列表添加类名控制样式
 
 - 动画
-    ```
-    yarn add animate.css --dev
-    https://daneden.github.io/animate.css/
-    https://github.com/daneden/animate.css
-    ```
 
-- svg皮肤背景色
-    ```
-    https://github.com/qrohlf/trianglify
-    
-    const canvas = Trianglify({
-                width: 1920,
-                height: 1080,
-                cell_size: Math.random()*200 + 40,
-                x_colors: 'random',
-                variance: Math.random(),
-            }).canvas();
-    ```
+  ```
+  yarn add animate.css --dev
+  https://daneden.github.io/animate.css/
+  https://github.com/daneden/animate.css
+  ```
+
+- svg 皮肤背景色
+
+  ```
+  https://github.com/qrohlf/trianglify
+
+  const canvas = Trianglify({
+              width: 1920,
+              height: 1080,
+              cell_size: Math.random()*200 + 40,
+              x_colors: 'random',
+              variance: Math.random(),
+          }).canvas();
+  ```
+
+  - 听人却吃饱饭，颜色稍稍调整 2022/9/21
 
 - snap.svg
-    ```
-    https://www.npmjs.com/package/snapsvg
-    yarn add snapsvg --dev
-    
-    官网：
-    
-    引入：import Snap from 'snapsvg';
-    报错：Uncaught TypeError: Cannot read property 'on' of undefined
-    解决：
-        yarn add imports-loader
-        1. 如此引用import Snap from 'imports-loader?this=>window,fix=>module.exports=0!snapsvg/dist/snap.svg.js'
-        2. 或修改webpack配置
-            module: {
-                    rules: [
-                        {
-                            test: require.resolve('snapsvg'),
-                            use: 'imports-loader?this=>window,fix=>module.exports=0',
-                        }
-                    ]
-            }
-    
-    使用：
-         var s = Snap("#svg");
-         var bigCircle = s.circle(150, 150, 100);
-    ```
+
+  ```
+  https://www.npmjs.com/package/snapsvg
+  yarn add snapsvg --dev
+
+  官网：
+
+  引入：import Snap from 'snapsvg';
+  报错：Uncaught TypeError: Cannot read property 'on' of undefined
+  解决：
+      yarn add imports-loader
+      1. 如此引用import Snap from 'imports-loader?this=>window,fix=>module.exports=0!snapsvg/dist/snap.svg.js'
+      2. 或修改webpack配置
+          module: {
+                  rules: [
+                      {
+                          test: require.resolve('snapsvg'),
+                          use: 'imports-loader?this=>window,fix=>module.exports=0',
+                      }
+                  ]
+          }
+
+  使用：
+       var s = Snap("#svg");
+       var bigCircle = s.circle(150, 150, 100);
+  ```
 
 - Elastic Progress
-    ```
-    
-    ```
+
+  ```
+
+  ```
 
 - TweenMax
-    ```
-    官网：http://greensock.com/
-    Github:https://github.com/greensock/GreenSock-JS/
-    
-    yarn add gsap --dev
-    
-    引入：
-    import { TweenMax } from "gsap/TweenMax";
-    ```
+
+  ```
+  官网：http://greensock.com/
+  Github:https://github.com/greensock/GreenSock-JS/
+
+  yarn add gsap --dev
+
+  引入：
+  import { TweenMax } from "gsap/TweenMax";
+  ```
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { Icon,Input,Badge,Menu, Dropdown,Divider, message } from 'antd';
+import { Icon, Input, Badge, Menu, Dropdown, Divider, message } from 'antd';
 import Trianglify from 'Trianglify';   //svg背景
-import { Link,withRouter } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import LeftBar from '../Bar/LeftBar';
@@ -9,242 +9,238 @@ import RightBar from '../Bar/RightBar';
 import adminImg from '../../assert/images/admin.jpg';
 
 import axios from 'axios';
-import { getSongsList,getCurrentItem } from "../../reducer/player.redux";
+import { getSongsList, getCurrentItem } from "../../reducer/player.redux";
 import { setLoginData } from "../../reducer/login.redux";
 import { getUrlFix, setItem } from '../../common/util';
 
 @withRouter
-	@connect(
-		state => state,
-		{ getSongsList, getCurrentItem, setLoginData }
-	)
+@connect(
+  state => state,
+  { getSongsList, getCurrentItem, setLoginData }
+)
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            height:document.getElementById('root').offsetHeight,
+  constructor(props) {
+    super(props);
+    this.state = {
+      height: document.getElementById('root').offsetHeight,
 
-            toggle: false,
-            avatarDefault: adminImg,
-            search: false,   //搜索
-            showSkin: false,  //皮肤
-            skinColor: 'themsBlack',
-            skinNormal: [
-                {color: 'themsBlack', name: '夜间'},
-            ],
-            skinSvg: [
-                {color: 'themsSVGL', name: '蓝色'},
-                // {color: 'themsSVGLF', name: '蓝粉色'},
-                {color: 'themsSVGLFH', name: '蓝粉色'},
-                {color: 'themsSVGLFZ', name: '蓝粉紫色'},
-                {color: 'themsSVGF', name: '粉色'},
-                {color: 'themsSVGFZ', name: '粉紫色'},
-                {color: 'themsSVGGF', name: '橘粉色'},
-            ],
+      toggle: false,
+      avatarDefault: adminImg,
+      search: false,   //搜索
+      showSkin: false,  //皮肤
+      skinColor: 'themsSVGLFH',
+      skinNormal: [
+        { color: 'themsSVGLFH', name: '夜间' },
+      ],
+      skinSvg: [
+        { color: 'themsSVGL', name: '蓝色' },
+        { color: 'themsSVGLFH', name: '蓝粉色' },
+        { color: 'themsSVGFZ', name: '紫色' },
+        { color: 'themsSVGLFZ', name: '粉紫色' },
+        // { color: 'themsSVGF', name: '粉色' },
+        { color: 'themsSVGGF', name: '粉紫色2' },
+      ],
 
-			skinSvgColorList:{
-                '蓝色':["#f7fbff", "#deebf7", "#c6dbef", "#9ecae1", "#6baed6", "#4292c6", "#2171b5", "#08519c", "#08306b"],
-                // '蓝粉色':["#fff7fb", "#ece7f2", "#d0d1e6", "#a6bddb", "#74a9cf", "#3690c0", "#0570b0", "#045a8d", "#023858"],
-                '蓝粉色':["#fff7fb", "#ece7f2", "#d0d1e6", "#a6bddb", "#74a9cf", "#3690c0", "#0570b0", "#045a8d", "#023858"],
-                '蓝粉紫色':["#f7fcfd", "#e0ecf4", "#bfd3e6", "#9ebcda", "#8c96c6", "#8c6bb1", "#88419d", "#810f7c", "#4d004b"],
+      skinSvgColorList: {
+        '蓝色': ["#08306b", "#08519c", "#2171b5", "#4292c6", "#6baed6", "#9ecae1", "#c6dbef", "#deebf7", "#f7fbff"],
+        '蓝粉色': ['#023858', '#045a8d', '#0570b0', '#3690c0', '#74a9cf', '#a6bddb', '#d0d1e6', '#ece7f2', '#fff7fb'],
+        '粉紫色': ['#4d004b', '#810f7c', '#88419d', '#8c6bb1', '#8c96c6', '#9ebcda', '#bfd3e6', '#e0ecf4', '#f7fcfd'],
 
-                '粉色':["#f7f4f9", "#e7e1ef", "#d4b9da", "#c994c7", "#df65b0", "#e7298a", "#ce1256", "#980043", "#67001f"],
-                '粉紫色':["#fcfbfd", "#efedf5", "#dadaeb", "#bcbddc", "#9e9ac8", "#807dba", "#6a51a3", "#54278f", "#3f007d"],
-                '橘粉色':["#fff7f3", "#fde0dd", "#fcc5c0", "#fa9fb5", "#f768a1", "#dd3497", "#ae017e", "#7a0177", "#49006a"],
+        // '粉色': ['#67001f', '#980043', '#ce1256', '#e7298a', '#df65b0', '#c994c7', '#d4b9da', '#e7e1ef', '#f7f4f9'],
+        '紫色': ['#3f007d', '#54278f', '#6a51a3', '#807dba', '#9e9ac8', '#bcbddc', '#dadaeb', '#efedf5', '#fcfbfd'],
+        '粉紫色2': ['#49006a', '#7a0177', '#ae017e', '#dd3497', '#f768a1', '#fa9fb5', '#fcc5c0', '#fde0dd', '#fff7f3'],
+        '橘色': ['#67000d', '#a50f15', '#cb181d', '#ef3b2c', '#fb6a4a', '#fc9272', '#fcbba1', '#fee0d2', '#fff5f0'],
+        '灰白色': ['#000000', '#252525', '#525252', '#737373', '#969696', '#bdbdbd', '#d9d9d9', '#f0f0f0', '#ffffff'],
 
+      },
+      showMusic: false,       //是否可拥有音乐功能
+      isPlay: false           //是否播放状态
+    };
+  }
 
-                '橘色':["#fff5f0", "#fee0d2", "#fcbba1", "#fc9272", "#fb6a4a", "#ef3b2c", "#cb181d", "#a50f15", "#67000d"],
-                '灰白色':["#ffffff", "#f0f0f0", "#d9d9d9", "#bdbdbd", "#969696", "#737373", "#525252", "#252525", "#000000"],
-
-            },
-            showMusic: false,       //是否可拥有音乐功能
-            isPlay: false           //是否播放状态
-        };
+  componentDidMount() {
+    if (this.state.skinColor != 'themsBlack') {
+      const canvas = Trianglify({
+        width: 2920,
+        height: this.state.height,
+        // cell_size: Math.random()*200 + 40,
+        // variance:0.98,
+        x_colors: ["#fff7fb", "#ece7f2", "#d0d1e6", "#a6bddb", "#74a9cf", "#3690c0", "#0570b0", "#045a8d", "#023858"]
+      });
+      document.getElementById('root').appendChild(canvas.canvas());
+      // console.log(canvas.opts.x_colors);
     }
+  }
 
-    componentDidMount() {
-        if(this.state.skinColor != 'themsBlack') {
-            const canvas = Trianglify({
-                width: 2920,
-                height: this.state.height,
-                // cell_size: Math.random()*200 + 40,
-                // variance:0.98,
-                x_colors: ["#fff7fb", "#ece7f2", "#d0d1e6", "#a6bddb", "#74a9cf", "#3690c0", "#0570b0", "#045a8d", "#023858"]
+  //获取歌曲MP3地址
+  getCurrenturl = (currentIndex) => {
+    const { currentId } = this.props.player;
+    if (currentId != null && currentId !== undefined) {
+      axios.get(`${getUrlFix}/music/url?id=${currentId}`).then(res => {
+        if (res.status == 200) {
+          if (res.code == 200) {
+            const currentUrl = res.data.data[0].url;
+            const item = { currentId, currentIndex, currentUrl };
+            this.props.getCurrentItem(item);
+            // console.log(currentId + " :: " + currentUrl);
+          } else {
+            message.warning(res.data.msg);
+          }
+
+        }
+      });
+    } else {
+      console.log(currentId)
+    }
+  }
+
+  //展示操作按钮
+  player = () => {
+    //判断是否登录
+    const { userName } = this.props.login;
+    /*if(!this.state.showMusic){
+        // if(userName !== ''){
+            //已登录
+            //获取歌单列表
+            axios.get(`${getUrlFix}/user/playlist?uid=262606203`).then(res=>{
+                //获取第一个歌单列表歌曲
+                this.props.getSongsList(1, res.data.playlist[0].id);
+
+                //初始化获取第一条歌曲
+                this.getCurrenturl(0);
+
+
             });
-            document.getElementById('root').appendChild(canvas.canvas());
-            // console.log(canvas.opts.x_colors);
+        // }else{
+        //     console.log('您未登录')
+        // }
+}*/
+    this.setState({ showMusic: !this.state.showMusic });
+  }
+
+  //播放
+  play = () => {
+    const audio = this.refs.audio;
+    this.setState({ isPlay: true });
+    audio.play();
+  }
+
+  //暂停
+  pause = () => {
+    const audio = this.refs.audio;
+    audio.pause();
+    this.setState({ isPlay: false, showMusic: false });
+  }
+
+  //更改皮肤svg
+  setSkin = (item) => {
+    this.setState({ skinColor: item.color });
+    //是svg背景
+    let oldCanvas = document.getElementById('root').children[1];
+    if (oldCanvas) {
+      document.getElementById('root').removeChild(oldCanvas);
+    }
+    const canvas = Trianglify({
+      width: 2920,
+      height: this.state.height,
+      x_colors: this.state.skinSvgColorList[item.name],
+    });
+    document.getElementById('root').appendChild(canvas.canvas())
+  }
+
+  signOut = () => {
+    setItem('userData', {});
+    this.props.setLoginData('', '')
+    this.props.history.push('/login');
+  }
+
+  render() {
+    const Search = Input.Search;
+    const { userName, avatar } = this.props.login;
+    let { currentUrl } = this.props.player;
+
+    const {
+      avatarDefault, search, toggle,
+      skinColor, skinNormal, skinSvg,
+      showMusic, isPlay
+    } = this.state;
+
+    const menu =
+      <Menu>
+        <Menu.SubMenu title="基础色皮肤">
+          {skinNormal.map((item, index) => {
+            return <Menu.Item key={`n-${index}`} onClick={() => this.setState({ skinColor: item.color })}>{item.name}</Menu.Item>
+          })}
+        </Menu.SubMenu>
+        <Menu.SubMenu title="svg格式皮肤">
+          {skinSvg.map((item, index) => {
+            return <Menu.Item key={`s-${index}`} onClick={() => this.setSkin(item)}>{item.name}</Menu.Item>
+          })}
+        </Menu.SubMenu>
+      </Menu>;
+
+    const logins = (
+      <Menu>
+        {userName == '' && avatar == '' ?
+          <Menu.Item><div className="login-in" onClick={() => this.props.history.push('/login')}><Icon type="smile" />登录</div></Menu.Item>
+          :
+          <Menu.Item><div className="login-out" onClick={this.signOut}><Icon type="poweroff" />退出</div></Menu.Item>
         }
-    }
-
-    //获取歌曲MP3地址
-    getCurrenturl = (currentIndex) => {
-        const {currentId} = this.props.player;
-        if (currentId != null && currentId !== undefined) {
-            axios.get(`${getUrlFix}/music/url?id=${currentId}`).then(res => {
-                if (res.status == 200) {
-                    if(res.code == 200) {
-                        const currentUrl = res.data.data[0].url;
-                        const item = {currentId, currentIndex, currentUrl};
-                        this.props.getCurrentItem(item);
-                        // console.log(currentId + " :: " + currentUrl);
-                    } else {
-                        message.warning(res.data.msg);
-                    }
-                    
-                }
-            });
-        } else {
-            console.log(currentId)
-        }
-    }
-
-    //展示操作按钮
-    player = () => {
-        //判断是否登录
-        const {userName} = this.props.login;
-        /*if(!this.state.showMusic){
-            // if(userName !== ''){
-                //已登录
-                //获取歌单列表
-                axios.get(`${getUrlFix}/user/playlist?uid=262606203`).then(res=>{
-                    //获取第一个歌单列表歌曲
-                    this.props.getSongsList(1, res.data.playlist[0].id);
-
-                    //初始化获取第一条歌曲
-                    this.getCurrenturl(0);
-
-
-                });
-            // }else{
-            //     console.log('您未登录')
-            // }
-		}*/
-        this.setState({showMusic: !this.state.showMusic});
-    }
-
-    //播放
-    play = () => {
-        const audio = this.refs.audio;
-        this.setState({isPlay: true});
-        audio.play();
-    }
-
-    //暂停
-    pause = () => {
-        const audio = this.refs.audio;
-        audio.pause();
-        this.setState({isPlay: false, showMusic: false});
-    }
-
-    //更改皮肤svg
-	setSkin = (item)=>{
-        this.setState({skinColor: item.color});
-    	//是svg背景
-        let oldCanvas = document.getElementById('root').children[1];
-        if(oldCanvas) {
-            document.getElementById('root').removeChild(oldCanvas);
-        }
-        const canvas = Trianglify({
-            width: 2920,
-            height: this.state.height,
-            x_colors: this.state.skinSvgColorList[item.name]
-        });
-        document.getElementById('root').appendChild(canvas.canvas())
-	}
-
-    signOut = () => {
-        setItem('userData', {});
-        this.props.setLoginData('', '')
-        this.props.history.push('/login');
-    }
-
-	render() {
-        const Search = Input.Search;
-        const { userName, avatar } = this.props.login;
-        let { currentUrl } = this.props.player;
-
-        const {
-        	avatarDefault, search,toggle,
-            skinColor, skinNormal, skinSvg,
-            showMusic,isPlay
-        } = this.state;
-
-        const menu  =
-            <Menu>
-                <Menu.SubMenu title="基础色皮肤">
-                    {skinNormal.map((item,index)=> {
-                        return <Menu.Item key={`n-${index}`} onClick={()=>this.setState({skinColor: item.color})}>{item.name}</Menu.Item>
-                    })}
-                </Menu.SubMenu>
-                <Menu.SubMenu title="svg格式皮肤">
-                    {skinSvg.map((item,index)=> {
-                        return <Menu.Item key={`s-${index}`} onClick={()=>this.setSkin(item)}>{item.name}</Menu.Item>
-                    })}
-                </Menu.SubMenu>
-            </Menu>;
-
-        const logins = (
-            <Menu>
-                {userName == '' && avatar == '' ?
-                    <Menu.Item><div className="login-in"  onClick={()=>this.props.history.push('/login')}><Icon type="smile" />登录</div></Menu.Item>
-                    :
-                    <Menu.Item><div className="login-out" onClick={this.signOut}><Icon type="poweroff" />退出</div></Menu.Item>
-                }
-            </Menu>
-        );
-		return(
-			<div className="lee-admin" style={{position:'absolute'}}>
-				<div className={`thems ${skinColor} ${toggle ? 'lee-in':''}`}>
-                    <LeftBar toggle={toggle}/>
-					<div className="lee-rightBar">
-						<div className="lee-rightBar-top">
-                            <div className="lee-rightBar-toogle" onClick={()=>{this.setState({toggle:!toggle})}}><Icon type={toggle ? 'menu-unfold':'menu-fold'} theme="outlined" /></div>
-							<div className="lee-rightBar-search">
-								<Search
-									style={{display:search ? null:'none'}}
-									placeholder="input search text"
-									onSearch={value => console.log(value)}
-								/>
-								<span>
-									<Icon
-										type="search"
-										onClick={()=>this.setState({search:!search})}
-									/>
-									搜索
-								</span>
-							</div>
-							<div className="lee-rightBar-top-r">
-								<div>
-									<Badge dot>
-										<Icon type="bell" />
-									</Badge>
-								</div>
-                                <div>
-									<Dropdown overlay={menu} trigger={['click']} placement='bottomRight'>
-										<span><Icon type="skin" style={{fontSize:20}} /></span>
-									</Dropdown>
-								</div>
-                                <div>
-                                    <Badge dot>
-										<Link to='/home:image'><Icon type="picture" theme="outlined" /></Link>
-                                    </Badge>
-                                </div>
-                                <Divider type="vertical" />
-                                <div className="user">
-                                    <Dropdown overlay={logins} overlayClassName="login-opera">
-                                        <a className="ant-dropdown-link" href="#">
-                                            <img src={avatar == '' ? avatarDefault : avatar} alt=""/>
-                                        </a>
-                                    </Dropdown>
-                                </div>
-                                <div className="name">
-                                    {userName}
-                                </div>
-							</div>
-						</div>
-                        <RightBar/>
-                        {/*animated infinite rubberBand jello*/}
-						{/* <div className="lee-music-opera" onClick={this.player}>
+      </Menu>
+    );
+    return (
+      <div className="lee-admin" style={{ position: 'absolute' }}>
+        <div className={`thems ${skinColor} ${toggle ? 'lee-in' : ''}`}>
+          <LeftBar toggle={toggle} />
+          <div className="lee-rightBar">
+            <div className="lee-rightBar-top">
+              <div className="lee-rightBar-toogle" onClick={() => { this.setState({ toggle: !toggle }) }}><Icon type={toggle ? 'menu-unfold' : 'menu-fold'} theme="outlined" /></div>
+              <div className="lee-rightBar-search">
+                <Search
+                  style={{ display: search ? null : 'none' }}
+                  placeholder="input search text"
+                  onSearch={value => console.log(value)}
+                />
+                <span>
+                  <Icon
+                    type="search"
+                    onClick={() => this.setState({ search: !search })}
+                  />
+                  搜索
+                </span>
+              </div>
+              <div className="lee-rightBar-top-r">
+                <div>
+                  <Badge dot>
+                    <Icon type="bell" />
+                  </Badge>
+                </div>
+                <div>
+                  <Dropdown overlay={menu} trigger={['click']} placement='bottomRight'>
+                    <span><Icon type="skin" style={{ fontSize: 20 }} /></span>
+                  </Dropdown>
+                </div>
+                <div>
+                  <Badge dot>
+                    <Link to='/home:image'><Icon type="picture" theme="outlined" /></Link>
+                  </Badge>
+                </div>
+                <Divider type="vertical" />
+                <div className="user">
+                  <Dropdown overlay={logins} overlayClassName="login-opera">
+                    <a className="ant-dropdown-link" href="#">
+                      <img src={avatar == '' ? avatarDefault : avatar} alt="" />
+                    </a>
+                  </Dropdown>
+                </div>
+                <div className="name">
+                  {userName}
+                </div>
+              </div>
+            </div>
+            <RightBar />
+            {/*animated infinite rubberBand jello*/}
+            {/* <div className="lee-music-opera" onClick={this.player}>
 							<div className={`${showMusic ? 'lee-music-animate':'in'}`}>
 								<Icon
 									type="customer-service" theme="outlined"
@@ -254,7 +250,7 @@ class App extends Component {
 							</div>
 						</div> */}
 
-						{/* <div className="lee-music-opera2" style={{display:showMusic ? 'block':'none'}}>
+            {/* <div className="lee-music-opera2" style={{display:showMusic ? 'block':'none'}}>
 							<audio
 								src={currentUrl}
 								ref='audio'
@@ -281,11 +277,11 @@ class App extends Component {
 									/>
 							</div>
 						</div> */}
-					</div>
-				</div>
-			</div>
-			)
-	}
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
